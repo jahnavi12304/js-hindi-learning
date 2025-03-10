06c2aca96cf56c6808686e6c572f7dc7ce939751
JAHNAVI JAIN <168407535+jahnavi12304@users.noreply.github.com>
2025-03-10 08:18:10 +0000

    modifications

---
+++ 01_BASICS/04_operatons.js
@@ -0,0 +1,33 @@
+/*
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+console.log(2+2)
+console.log(2*2)
+console.log(2-2)
+console.log(2**3)
+console.log(8/2)
+console.log(3%2)
+
+let str_1 = "hello"
+
+let str_2 = " hitesh"
+
+let str_3 = str_1 +str_2
+console.log(str_3);
+*/
+console.log("2" + 1)
+console.log(1+ "2" )
+console.log("2" + 1 +1)
+console.log(2 + 3 +"2" + 1)
+
+
+
+
+
+
+
+
+
+
