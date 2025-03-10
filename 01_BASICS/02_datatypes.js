7d3b74124fb490d784ed3158b4250090c8bce1d5
JAHNAVI JAIN <168407535+jahnavi12304@users.noreply.github.com>
2025-03-10 05:29:03 +0000

    datatypes

---
+++ 01_BASICS/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // for using the newer version of the javascript code 
+/*
+alert(3+3) we don't use this , this can be possible in the browser under the console
+ section but currently we are using node and nothing like thst is specified 
+*/
+let name = "Aditya"
+let age = 21
+let isLoggedIn = true
+let state;
+let city = null
+
+console.log(typeof "Aditya")
+console.log(typeof age)
+console.log(typeof isLoggedIn )
+console.log(typeof state)
+console.log(typeof city)
+
+/*
+string
+number 
+boolen 
+bigint
+ null when we use type of it gives object 
+undefined when we use type of it gives undefined
+object
+*/
